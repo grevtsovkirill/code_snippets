60cc1f444ccd347491084899873e6cd6722a7792
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-03-26 14:38:34 +0100

    cleaning commented items

--- py_scripts/merger.py
+++ py_scripts/merger.py
@@ -5,7 +5,6 @@ samples = ['413008_ttw','410155_ttw','410472_ttbar','410156_ttZnunu','410157_ttZ
            '410658_topt','410659_atopt','410644_tops','410645_atops',           
            '304014_threeTop','410080_fourTop','410081_ttww','410408_WtZ']
 #samples = ['410155_ttw']                                                                                                                                                      
-#samples = ['410472_ttbar']
 for itt in samples:
     dsid_cut = itt.find("_")
     dsid = itt[:dsid_cut]
