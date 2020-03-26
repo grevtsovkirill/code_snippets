08a19ae8912ce955570cd6486e1ae6e87cca81eb
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-03-26 14:35:53 +0100

    extract DSID from mypath variable

--- py_scripts/split_hadd.py
+++ py_scripts/split_hadd.py
@@ -12,21 +12,21 @@ mypath = vars(args)["path"]
 n_split = vars(args)["nsplit"]
 
 #mypath = 'user.kgrevtso.410470.PhPy8EG.DAOD_TOPQ1.e6337_s3126_r10201_p3832.1103_MCTrC_v0_output_root'
+dsid = mypath[14:20]
+
 onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 n_div=len(onlyfiles)/n_split+1
 print(n_div)
 
 lll = [onlyfiles[i:i + n_div] for i in range(0, len(onlyfiles),n_div )]
 
-#print(lll[0])
-#l_val = 0
 for l_val in range(n_split): 
     
-    command = 'hadd 410470_'+str(l_val)+'.root '
+    command = 'hadd '+dsid+'_'+str(l_val)+'.root '
     for it in lll[l_val]:
         command+=mypath+'/'+it+' '
 
-    print(command)
+    #print(command)
     print(" ========================== ")
-    #system(command)
+    system(command)
     print(" ------------------------- ")
